aae9f38c1994154c5940789d8499638a1fd407a8
Aleksandr <elfcorp@gmail.com>
2016-11-02 19:47:34 +0500

    Selenium grid option added

--- Tests/src/Utils/Utils.java
+++ Tests/src/Utils/Utils.java
@@ -3,25 +3,36 @@ package Utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Utils {
     public static WebDriver getWebDriver() {
         String driver = System.getProperty("testdriver.env", "chrome");
+        String gridUrl = System.getProperty("remoteurl.env", "http://localhost:4444/wd/hub");
 
         switch (driver)
         {
             case "chrome":
                 return new ChromeDriver();
+            case "chromeGrid":
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setBrowserName("chrome");
+                try {
+                    return new RemoteWebDriver(new URL(gridUrl), capabilities);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             case "phantomJs":
                 return new PhantomJSDriver();
             case "firefox":
                 return new FirefoxDriver();
-            case "htmlunit":
-                return new HtmlUnitDriver(true);
             default:
-                return new HtmlUnitDriver(true);
+                return new ChromeDriver();
         }
     }
 }
